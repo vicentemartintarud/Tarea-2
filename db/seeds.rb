e988c3be3e359e5633665fdaf58e874825b9289a
Vicente Martin <vicentemartintarud@gmail.com>
2017-03-29 22:14:07 -0400

    Seed
    Creación de seed de usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'vmartin', first_name: 'Vicente', last_name: 'Martin', twitter:'vmartint')
+User.create(username: 'frenz', first_name: 'Fernando', last_name: 'Renz', twitter:'frenz')
+User.create(username: 'prenz', first_name: 'Felipe', last_name: 'Renz', twitter:'perenz')
+User.create(username: 'nrenz', first_name: 'Nicolás', last_name: 'Renz')
+User.create(username: 'mono', first_name: 'Adrián')
+User.create(username: 'enaco', first_name: 'Sven', twitter:'svrichter')
